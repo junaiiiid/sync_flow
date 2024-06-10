27eeb4c60aeb96b927b46af4920a8c3c387e9941
junaidtariq <junaidt805@gmail.com>
2024-06-11 03:12:41 +0500

    loader added to comments screen

--- lib/features/comments/ui/comments_screen.dart
+++ lib/features/comments/ui/comments_screen.dart
@@ -3,6 +3,7 @@ import 'package:flow_sync/constants/app_assets.dart';
 import 'package:flow_sync/constants/enums.dart';
 import 'package:flow_sync/constants/extensions.dart';
 import 'package:flow_sync/features/comments/ui/widgets/bubble.dart';
+import 'package:flow_sync/features/comments/ui/widgets/comment_card.dart';
 import 'package:flow_sync/features/comments/view_model/comment_view_model.dart';
 import 'package:flow_sync/features/dashboard/model/comment_model.dart';
 import 'package:flow_sync/features/home/ui/widgets/custom_app_bar.dart';
@@ -32,165 +33,20 @@ class CommentsScreen extends ConsumerWidget {
               appBar: CustomAppBar.appBarWithBackButton(title: "Comments"),
               body: Padding(
                 padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
-                child: viewModel.allComments.isEmpty
-                    ? CircularProgressIndicator()
-                    : ListView(
-                        physics: const BouncingScrollPhysics(),
-                        children: viewModel.allComments
-                            .map<Widget>((comment) => commentCard(
-                                model: comment, viewModel: viewModel))
-                            .toList(),
-
-                        /*[
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        "Project Name",
-                        style: AppTextStyles.titleSmall,
-                      ),
-                    ),
-                    commentCard(),
-                    commentCard()
-                  ],*/
-                      ),
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: (viewModel.allComments.isEmpty)
+                      ? [const CommentCardSkeleton()]
+                      : viewModel.allComments
+                          .map<Widget>((comment) =>
+                              CommentCard(model: comment, viewModel: viewModel))
+                          .toList(),
+                ),
               ),
             ),
           ),
         );
       },
-    );
-  }
-
-  Widget commentCard(
-      {required Comment model, required CommentViewModel viewModel}) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 10.h),
-      child: Card(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 25.h, horizontal: 25.w),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      "Comment# ${model.id}",
-                      style: AppTextStyles.labelSmall,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          /*backgroundColor: viewModel
-                              .getProjectById(id: model.id)
-                              .color
-                              .toColor(),*/
-                          radius: 10.r,
-                        ),
-                        SizedBox(
-                          width: 10.w,
-                        ),
-                        Text(
-                          viewModel.getTaskById(id: model.taskId).content,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          softWrap: true,
-                          style: AppTextStyles.labelSmall,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.h),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    model.postedAt.toString().toFormattedDate(),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    softWrap: true,
-                    style: AppTextStyles.labelSmall,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 20.h),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Flex(
-                    direction: Axis.horizontal,
-                    children: [
-                      Expanded(
-                        flex: 0,
-                        child: CircleAvatar(
-                          backgroundColor: AppColors.darkGrey,
-                          child: SvgPicture.asset(AppAssets.userIcon),
-                        ),
-                      ),
-                      Expanded(
-                          flex: 3,
-                          child: Bubble(
-                              text: model.content, color: AppColors.lightGrey)),
-                    ],
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Visibility(
-                      visible: (model.attachment != null),
-                      child: commentAction(
-                          type: CommentActionType.viewAttachment, model: model, viewModel: viewModel)),
-                  commentAction(type: CommentActionType.editComment, model: model, viewModel: viewModel),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget commentAction(
-      {required CommentActionType type,
-      required Comment model,
-      required CommentViewModel viewModel}) {
-    late IconData iconData;
-    late String title;
-    late Function() onTap;
-    switch (type) {
-      case CommentActionType.viewAttachment:
-        iconData = Icons.link;
-        title = "View Attachment";
-        onTap = () {
-          viewModel.openAttachment(url: model.attachment?.fileUrl ?? "");
-        };
-      case CommentActionType.editComment:
-        iconData = Icons.edit;
-        title = "Edit Comment";
-        onTap = () {};
-    }
-    return InkWell(
-      onTap: onTap,
-      child: Row(
-        children: [
-          Icon(iconData),
-          SizedBox(
-            width: 5.w,
-          ),
-          Text(
-            title,
-            style: AppTextStyles.labelSmall,
-          )
-        ],
-      ),
     );
   }
 }
---
+++ lib/features/comments/ui/widgets/comment_card.dart
@@ -0,0 +1,284 @@
+import 'package:flow_sync/constants/extensions.dart';
+import 'package:flow_sync/features/comments/view_model/comment_view_model.dart';
+import 'package:flow_sync/features/dashboard/model/comment_model.dart';
+import 'package:flow_sync/global_widgets/skeleton_effect_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../constants/app_assets.dart';
+import '../../../../constants/enums.dart';
+import '../../../../styles_and_themes/app_colors.dart';
+import '../../../../styles_and_themes/app_text_styles.dart';
+import 'bubble.dart';
+
+class CommentCard extends StatelessWidget {
+  final Comment model;
+  final CommentViewModel viewModel;
+  const CommentCard({super.key, required this.model, required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.h),
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 25.h, horizontal: 25.w),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Comment# ${model.id}",
+                      style: AppTextStyles.labelSmall,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          /*backgroundColor: viewModel
+                              .getProjectById(id: model.id)
+                              .color
+                              .toColor(),*/
+                          radius: 10.r,
+                        ),
+                        SizedBox(
+                          width: 10.w,
+                        ),
+                        Text(
+                          viewModel.getTaskById(id: model.taskId).content,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                          softWrap: true,
+                          style: AppTextStyles.labelSmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.h),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    model.postedAt.toString().toFormattedDate(),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    softWrap: true,
+                    style: AppTextStyles.labelSmall,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.h),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Flex(
+                    direction: Axis.horizontal,
+                    children: [
+                      Expanded(
+                        flex: 0,
+                        child: CircleAvatar(
+                          backgroundColor: AppColors.darkGrey,
+                          child: SvgPicture.asset(AppAssets.userIcon),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 3,
+                          child: Bubble(
+                              text: model.content, color: AppColors.lightGrey)),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Visibility(
+                      visible: (model.attachment != null),
+                      child: commentAction(
+                          type: CommentActionType.viewAttachment, model: model, viewModel: viewModel)),
+                  commentAction(type: CommentActionType.editComment, model: model, viewModel: viewModel),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget commentAction(
+      {required CommentActionType type,
+        required Comment model,
+        required CommentViewModel viewModel}) {
+    late IconData iconData;
+    late String title;
+    late Function() onTap;
+    switch (type) {
+      case CommentActionType.viewAttachment:
+        iconData = Icons.link;
+        title = "View Attachment";
+        onTap = () {
+          viewModel.openAttachment(url: model.attachment?.fileUrl ?? "");
+        };
+      case CommentActionType.editComment:
+        iconData = Icons.edit;
+        title = "Edit Comment";
+        onTap = () {};
+    }
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        children: [
+          Icon(iconData),
+          SizedBox(
+            width: 5.w,
+          ),
+          Text(
+            title,
+            style: AppTextStyles.labelSmall,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CommentCardSkeleton extends StatelessWidget {
+  const CommentCardSkeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.h),
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 25.h, horizontal: 25.w),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: SkeletonEffectWidget(
+                      child: Text(
+                        "Loading...",
+                        style: AppTextStyles.labelSmall,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Row(
+                      children: [
+                        SkeletonEffectWidget(
+                          child: CircleAvatar(
+                            radius: 10.r,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.w,
+                        ),
+                        SkeletonEffectWidget(
+                          child: Text(
+                            "Loading...", 
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            softWrap: true,
+                            style: AppTextStyles.labelSmall,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.h),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: SkeletonEffectWidget(
+                    child: Text(
+                      "Please Wait...",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      softWrap: true,
+                      style: AppTextStyles.labelSmall,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.h),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Flex(
+                    direction: Axis.horizontal,
+                    children: [
+                      Expanded(
+                        flex: 0,
+                        child: CircleAvatar(
+                          backgroundColor: AppColors.darkGrey,
+                          child: SvgPicture.asset(AppAssets.userIcon),
+                        ),
+                      ),
+                      const Expanded(
+                          flex: 3,
+                          child: SkeletonEffectWidget(
+                            child: Bubble(  
+                                text: "Please Wait...", color: AppColors.lightGrey),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SkeletonEffectWidget(child: Icon(Icons.link)),
+                      SizedBox(
+                        width: 5.w,
+                      ),
+                      SkeletonEffectWidget(
+                        child: Text(
+                          "Loading...",
+                          style: AppTextStyles.labelSmall,
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const SkeletonEffectWidget(child: Icon(Icons.edit)),
+                      SizedBox( 
+                        width: 5.w,
+                      ),
+                      SkeletonEffectWidget(
+                        child: Text( 
+                          "Loading...",
+                          style: AppTextStyles.labelSmall,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
